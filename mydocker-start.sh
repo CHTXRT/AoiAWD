c8112c3d73caaf6eb3b615406b6a504572b12747
天翔 <1931818642@qq.com>
2025-02-10 14:18:35 +0800

    chore: Update mydocker-start.sh script（可以使用脚本启动docker，建议使用）
    The `mydocker-start.sh` script has been updated to include commands that copy the compiled files (`tapeworm.phar`, `roundworm`, and `guardian.phar`) from the Docker container to the project directory. This change ensures that the compiled files are easily accessible for further use.

--- mydocker-start.sh
+++ mydocker-start.sh
@@ -1,5 +1,19 @@
+cd `dirname $0`
+# pwd
+docker-compose up -d --build
+docker logs aoiawd-aoiawd-1
+
 docker exec aoiawd-aoiawd-1 "apt-get update"
 docker exec aoiawd-aoiawd-1 "apt-get install libinotifytools0-dev"
 docker exec -w /usr/src/TapeWorm aoiawd-aoiawd-1 "php compile.php"
 docker exec -w /usr/src/RoundWorm aoiawd-aoiawd-1 "make"
 docker exec -w /usr/src/Guardian aoiawd-aoiawd-1 "php compile.php"
+
+echo ""
+docker cp aoiawd-aoiawd-1:/usr/src/TapeWorm/tapeworm.phar .
+echo "tapeworm.phar 已复制到项目目录"
+docker cp aoiawd-aoiawd-1:/usr/src/RoundWorm/roundworm .
+echo "roundworm 已复制到项目目录"
+docker cp aoiawd-aoiawd-1:/usr/src/Guardian/guardian.phar .
+echo "guardian.phar 已复制到项目目录"
+echo ""
