81583f0cec3a1978e8297eb2b0ff7a30dcf07aa6
天翔 <1931818642@qq.com>
2025-02-10 19:17:00 +0800

    chore: Update mydocker-start.sh script
    The `mydocker-start.sh` script has been updated to improve the execution of commands within the Docker container. The changes include using the `-y` flag for the `apt-get install` command and removing unnecessary quotation marks around the commands. This update enhances the script's functionality and ensures a smoother execution process.

--- mydocker-start.sh
+++ mydocker-start.sh
@@ -3,11 +3,11 @@ cd `dirname $0`
 docker-compose up -d --build
 docker logs aoiawd-aoiawd-1
 
-docker exec aoiawd-aoiawd-1 "apt-get update"
-docker exec aoiawd-aoiawd-1 "apt-get install libinotifytools0-dev"
-docker exec -w /usr/src/TapeWorm aoiawd-aoiawd-1 "php compile.php"
-docker exec -w /usr/src/RoundWorm aoiawd-aoiawd-1 "make"
-docker exec -w /usr/src/Guardian aoiawd-aoiawd-1 "php compile.php"
+docker exec aoiawd-aoiawd-1 apt-get update
+docker exec aoiawd-aoiawd-1 apt-get install -y libinotifytools0-dev
+docker exec -w /usr/src/TapeWorm aoiawd-aoiawd-1 php compile.php
+docker exec -w /usr/src/RoundWorm aoiawd-aoiawd-1 make
+docker exec -w /usr/src/Guardian aoiawd-aoiawd-1 php compile.php
 
 echo ""
 docker cp aoiawd-aoiawd-1:/usr/src/TapeWorm/tapeworm.phar .
